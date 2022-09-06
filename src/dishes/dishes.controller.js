80e1f2467f48c0226f53ef20406b1fb51639ab78
Alyssa Suphunthuchat <axsuphu@gmail.com>
2022-09-06 13:03:08 -0700

    completed post dishes router

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,56 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+
+function list(req, res, next) {
+  res.json({ data: dishes });
+}
+
+function bodyDataHas(propertyName) {
+  return function (req, res, next) {
+    const { data = {} } = req.body;
+    if (data[propertyName]) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Must include a ${propertyName}`,
+    });
+  };
+}
+
+function priceIsValid(req, res, next) {
+  const { data: { price } = {} } = req.body;
+  if (price <= 0 || !Number.isInteger(price)) {
+    return next({
+      status: 400,
+      message: `price requires a valid number`,
+    });
+  }
+  next();
+}
+
+function create(req, res) {
+  const { data: { name, description, price, image_url } = {} } = req.body;
+  const newDish = {
+    id: nextId(),
+    name,
+    description,
+    price,
+    image_url,
+  };
+  dishes.push(newDish);
+  return res.status(201).json({ data: newDish });
+}
+
+module.exports = {
+  list,
+  create: [
+    bodyDataHas("name"),
+    bodyDataHas("price"),
+    bodyDataHas("description"),
+    bodyDataHas("image_url"),
+    priceIsValid,
+    create,
+  ],
+};
